a1997a7f67ab6a485215a133e18e531aa08ed184
miguelange araujo <miguelange07araujo@gmail.com>
2024-02-14 01:02:21 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+
+    let envelope = $('#envelope');
+    let btn_open = $("#open");
+    let btn_reset = $("#reset");
+
+
+    envelope.click( function() {
+        open();
+    });
+    btn_open.click( function() {
+        open();
+    });
+    btn_reset.click( function() {
+        close();
+    });
+
+    function open() {
+        envelope.addClass("open")
+           .removeClass("close");
+    }
+    function close() {
+        envelope.addClass("close")
+           .removeClass("open");
+    }
+
+});
